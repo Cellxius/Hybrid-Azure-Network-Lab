275ed19b613911423fd761519c9a4edb8eda9f39
Marcellus Velsor <92658558+Cellxius@users.noreply.github.com>
2026-08-21 19:03:22 -0500

    Add pre-commit guard for private data

--- sanitize.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Usage: ./sanitize.sh infile outfile
-sed -E \
-  -e 's/104\.9\.148\.199/<ONPREM_PUBLIC_IP>/g' \
-  -e 's/104\.9\.148\.1/<ISP_GATEWAY>/g' \
-  -e 's/a8:40:f8:1d:19:c0/<GATEWAY_MAC>/gI' \
-  -e 's/D93LE5AU300157/<SERIAL>/g' \
-  -e 's/2001:506:[0-9a-f:]+/<IPV6_PREFIX>/gI' \
-  "$1" > "$2"
